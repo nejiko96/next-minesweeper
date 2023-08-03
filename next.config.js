94b768a5ab6a853a2b9f53dbb5331f187c769a2f
nejiko96 <nejiko2006@gmail.com>
2023-08-03 16:43:59 +0900

    next-translate configuration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
+const nextTranslate = require('next-translate-plugin')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+module.exports = nextTranslate(nextConfig)
