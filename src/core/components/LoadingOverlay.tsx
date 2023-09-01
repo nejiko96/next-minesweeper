a3ee41584d8472ff5260a7276d38f70468bbfedd
nejiko96 <nejiko2006@gmail.com>
2023-09-01 16:26:49 +0900

    add LoadingOverlay.tsx

---
+++ src/core/components/LoadingOverlay.tsx
@@ -0,0 +1,26 @@
+import { ColorRing } from 'react-loader-spinner'
+
+type Props = {
+  show: boolean
+  text?: string
+}
+
+const LoadingOverlay: React.FC<Props> = ({ show, text }) => {
+  return (
+    show && (
+      <div className="fixed left-0 top-0 z-50 flex h-full w-full flex-col items-center justify-center gap-3 overflow-auto bg-black/40">
+        <ColorRing
+          visible={true}
+          height="80"
+          width="80"
+          ariaLabel="blocks-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+          colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+        />
+        <p className="text-center">{text}</p>
+      </div>
+    )
+  )
+}
+export default LoadingOverlay
--- src/modules/catapi/views/CatView.tsx
+++ src/modules/catapi/views/CatView.tsx
@@ -3,6 +3,7 @@
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
 
+import LoadingOverlay from '@/core/components/LoadingOverlay'
 import { fetchCatImage } from '../models/catApi'
 
 const CatView: React.FC = () => {
@@ -24,23 +25,26 @@ const CatView: React.FC = () => {
   }, [])
 
   return (
-    <div className="p-4">
-      <button
-        className="mx-auto block cursor-pointer rounded border-2 border-transparent bg-red-500 px-4 py-2 font-semibold text-white transition duration-300 hover:border-red-300 hover:bg-red-600"
-        onClick={updateCatImage}
-      >
-        Today&apos;s Cat
-      </button>
-      <div className="relative mx-auto mt-4 h-screen w-3/5">
-        <Image
-          src={catImageUrl}
-          fill
-          style={{ objectFit: 'contain', objectPosition: '50% top' }}
-          alt="Cat Image"
-          onLoad={loadedCatImage}
-        />
+    <>
+      <LoadingOverlay show={isLoading} text="loading..." />
+      <div className="p-4">
+        <button
+          className="mx-auto block cursor-pointer rounded border-2 border-transparent bg-red-500 px-4 py-2 font-semibold text-white transition duration-300 hover:border-red-300 hover:bg-red-600"
+          onClick={updateCatImage}
+        >
+          Today&apos;s Cat
+        </button>
+        <div className="relative mx-auto mt-4 h-screen w-3/5">
+          <Image
+            src={catImageUrl}
+            fill
+            style={{ objectFit: 'contain', objectPosition: '50% top' }}
+            alt="Cat Image"
+            onLoadingComplete={loadedCatImage}
+          />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 export default CatView
